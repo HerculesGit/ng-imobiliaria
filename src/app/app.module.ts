5079d00461d2fa2c175c1616e8c7dc2277ebedc7
HerculesGit <hercules.silva@dce.ufpb.br>
2019-09-06 16:02:13 -0300

    T7.5 - Criar componente MenuComponent no AppComponent - criado e colocado no app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarClientesComponent } from './components/listar-clientes/listar-clientes.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarClientesComponent
+    ListarClientesComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
