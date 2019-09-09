8d1907b67066d23d53f16fb2545388908f6d50f6
HerculesGit <hercules.silva@dce.ufpb.br>
2019-09-09 20:18:39 -0300

    T7.8 - Rotas /clientes - Adicionado rotas para listar clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarClientesComponent } from './components/listar-clientes/listar-clientes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // sugestao de implementacao {path: '**', component: PageNotFoundComponent },
+  { path: 'clientes', component: ListarClientesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
