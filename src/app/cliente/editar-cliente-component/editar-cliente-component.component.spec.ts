942e7c7071df86e04d830df0d90fccf0afffff73
analiviafs <ana.livia@dce.ufpb.br>
2019-09-16 17:48:38 -0300

    T6.1 - Criar component e template EditarClienteComponent - componente com template criado

---
+++ src/app/cliente/editar-cliente-component/editar-cliente-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditarClienteComponentComponent } from './editar-cliente-component.component';
+
+describe('EditarClienteComponentComponent', () => {
+  let component: EditarClienteComponentComponent;
+  let fixture: ComponentFixture<EditarClienteComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditarClienteComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditarClienteComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
